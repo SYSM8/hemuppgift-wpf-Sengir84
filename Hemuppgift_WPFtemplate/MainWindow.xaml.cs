2ddc8b9b763f371c4e298a1892e969a9db4abf13
Alexander Larsson <alexander.larsson@edu.newton.se>
2024-10-06 22:33:31 +0200

    Knappar Mainwindow

--- Hemuppgift_WPFtemplate/MainWindow.xaml.cs
+++ Hemuppgift_WPFtemplate/MainWindow.xaml.cs
@@ -12,5 +12,29 @@ namespace Hemuppgift_WPFtemplate
         {
             InitializeComponent();
         }
+
+        private void BtnGrid_Click(object sender, RoutedEventArgs e)
+        {
+            GridWindow gridWindow = new GridWindow();
+            gridWindow.Show();
+        }
+
+        private void BtnStack_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Show();
+        }
+
+        private void BtnWrap_Click(object sender, RoutedEventArgs e)
+        {
+            WrapPanel wrapPanel = new WrapPanel();
+            wrapPanel.Show();
+        }
+
+        private void BtnDock_Click(object sender, RoutedEventArgs e)
+        {
+            DockPanel dockPanel = new DockPanel();
+            dockPanel.Show();
+        }
     }
 }
